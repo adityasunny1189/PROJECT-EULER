4083f7056fff3ffd3e2a9814c9e5c7530686fc3b
adityasunny1189 <adityapathak1189@gmail.com>
2020-01-20 13:38:49 +0530

    fibonacci

--- even_fibonachi.cpp
+++ even_fibonachi.cpp
@@ -1,17 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
+  long long int sum = 0;
   int count = 0;
   int fib1 = 0;
   int fib2 = 1;
-  while(count < 10) {
-    int fib_next = fib1 + fib2;
+  long long int fib_next = 0;
+  while(fib_next < 4000000) {
+    fib_next = fib1 + fib2;
+    if(fib_next % 2 == 0)
+      sum += fib_next;
     cout << fib_next << " ";
     fib1 = fib2;
     fib2 = fib_next;
     count++;
     }
     cout << endl;
+    cout << sum << endl;
   return 0;
 }
 
